2484af6cf79bdf17f46329c9c5418ef2d3c88c5e
Yarden Zeron <zeron.yarden@gmail.com>
2022-12-05 23:46:33 +0200

    customized to my profile use of emojis and change of info between games

--- js/main.js
+++ js/main.js
@@ -1,27 +1,86 @@
 'use strict'
 
+$(onInit)
+
+
+function onInit() {
+    renderProtfolio()
+    
+}
 
 
 function renderProtfolio() {
+    const projects=getProjectBack()
     var strHTMLs=''
-    `<div class="col-md-4 col-sm-6 portfolio-item">
-        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+    strHTMLs=projects.map(project=>
+    `
+    <div data="${project.id}" class="col-md-4 col-sm-6 portfolio-item px-3 projectRender" onClick="onInfo(this)">
+        <a class="portfolio-link try" data-toggle="modal" href="#portfolioModal">
             <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
-                    <i class="fa fa-plus fa-3x"></i>
+                    <i class="fa fa-plus fa-3x forInfo"></i>
                 </div>
             </div>
-            <img class="img-fluid" src="img/portfolio/01-thumbnail.jpg" alt="">
+            <img class="fitted img-fluid" src="img/projects/${project.id}-fitted.png" alt="project ${project.name}">
         </a>
         <div class="portfolio-caption">
             <h4>Threads</h4>
-            <p class="text-muted">Illustration</p>
+            <p class="text-muted"></p>
         </div>
-    </div>`
+    </div> 
+`
+    )
+    // strHTMLs.join('')
+    $('.Protfolios').html(strHTMLs)
+    // addCarEventListeners()
+}
 
+// function pushData(){
+//     console.log('ok:')
+// }
+
+function onInfo(info){
+    const carId =info.attributes.data.value
+    const project=getSingleProject(carId)
+    console.log('project:', project.id)
+    $(".modal-body h2 span").text(project.name)
+    $(".modal-body p span").text(project.title)
+    $("#bigPicture").attr('src',"img/projects/"+project.id+".png")
+    // $("#bigPicture").attr('src', $("#bigPicture").attr("src").replace(gLastImg, `${project.id}`))
+    $(".desc").text(project.desc)
+}
+
+$(".projectRender").on('click', function (e){
+    console.log('in:')
+})
+
+
+
+
+
+
+function addProjectEventListeners() {
+    // $('.btn-remove').on('click', function () {
+    //     const carId = $(this).closest('.car-preview').data('carId')
+    //     onDeleteCar(carId)
+    // })
+
+    // $('.btn-read').on('click', function () {
+    //     const carId = $(this).closest('.car-preview').data('carId')
+    //     onReadCar(carId)
+    // })
+
+    // $('.btn-update').on('click', function () {
+    //     const carId = $(this).closest('.car-preview').data('carId')
+    //     onUpdateCar(carId)
+    // })
 }
 
 function slideToContact() {
     $('html,body').animate({ scrollTop: $(".contacMe").offset().top }, 'slow')
     openCanvas()
 }
+
+
+
+// <img class="img-fluid" src="img/portfolio/${project.id}.jpg" alt="project ${project.name}">
--- js/serves.protfolio.js
+++ js/serves.protfolio.js
@@ -1,16 +1,73 @@
 'use strict'
 
+var projects={}
+var projectInfo
+const KEY='protfolioDB'
 
+function getProjectBack(){
+    _getProjectObject()
+    _getProjs()
+    return projects
+}
 
-function inti() {
+function getSingleProject(id){
+    const proj=projects.find(p=> p.id===id)
+    return proj
+}
+
+function _getProjectObject(){
+    projectInfo=[
+        {
+            id:"game1",
+            name:"Chaess",
+            title:"Chaess game to play one aginst the other",
+            desc: "Project who be start by the techers and was complited by me",
+            url: "https://www.google.co.il/",
+            lable:['Game', 'Thinking', 'Black and White']
+        },
+        {
+            id:"game2",
+            name:"Packman",
+            title:"Packman game who is custom to our days",
+            desc: "Project who be start by the techers and was complited by me",
+            url: "https://www.google.co.il/",
+            lable:['Packman', 'Time', 'Eat and win']
+        },
+        {
+            id:"game3",
+            name:"BookStore",
+            title:"Book list who can be controled by the bookkeeper",
+            desc: "My own book storage projecte made by me",
+            url: "https://www.google.co.il/",
+            lable:['Books', 'Adding', 'Read']
+        },
+        {
+            id:"game4",
+            name:"Mineswepper",
+            title:"Mine Swepper to play around and feel in the 90th's",
+            desc: "Mine swepper to make fun with your self and save the world",
+            url: "https://red-foramn.github.io/Mine-swapper/",
+            lable:['MineSwpper', 'Race', 'Until the death']
+        },
+    ]
+}
+
+function _getProjs(){
+    projects=loadFromStorage(KEY)
+    if (!projects){
+        projects=projectInfo.map(proj=>_createProject(
+            proj.id,
+            proj.name,
+            proj.title,
+            proj.desc,
+            proj.url,
+            proj.label
+        ))
+    }
 
 }
 
-function projList(){
-
-}
-
-function _createProject (id, name, title, dece, url, label){
+function _createProject (id, name, title, desc, url, label){
     return{
         id,
         name,
@@ -21,3 +78,5 @@ function _createProject (id, name, title, dece, url, label){
         label
     }
 }
+
+
