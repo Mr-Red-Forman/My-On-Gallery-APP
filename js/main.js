d77f4864b480be36c34529a34bd1588681c3cee3
Yarden Zeron <zeron.yarden@gmail.com>
2022-12-05 17:10:14 +0200

    Update my info, adding a contact section and traget it to when click on contact pic

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 console.log('Starting up');
+
+
+// function openCanvas(){
+//     $(".page-top").click(function(e) {
+//         e.preventDefault(e)
+//         $('html,body').animate({scrollTop: $(".contacMe").offset().top},'slow');
+// })
+// }
+
+function slideToContact(){
+    $('html,body').animate({scrollTop: $(".contacMe").offset().top},'slow')
+    openCanvas()
+}
+// $("#contact h4").click(function(e) {
+//     e.preventDefault()
+//     $('html,body').animate({scrollTop: $(".contacMe").offset().top},'slow')
+//     openCanvas()
+
+// })
